45fef1a3231049ab584bdd7de5dd67a48d339ef5
Sindhu012308 <sindhu012308@gmail.com>
2019-10-18 12:25:45 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DoctorComponent } from './doctor/doctor.component';
+import { ImagingComponent } from './imaging/imaging.component';
+import { LabsComponent } from './labs/labs.component';
+import { RecordsComponent } from './records/records.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    DoctorComponent,
+    ImagingComponent,
+    LabsComponent,
+    RecordsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
